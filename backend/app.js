3c4da55e5c4359787a08cdbd9e23d5d0960eda20
David Kerekes <davkedav@gmail.com>
2020-08-07 21:08:04 +0200

    refct: split index.js -> app.js server.js

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const router = require('./routes/router');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json({ limit: '500MB', type: 'application/json' }));
+app.use(express.urlencoded({ limit: '500MB', extended: true }));
+app.use(router);
+app.all('*', (req, res) => {
+  res.status(404);
+  res.send('<h3>Error 404: page not found</h3>');
+});
+
+module.exports = app;
