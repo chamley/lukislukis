95e5672200b6dde44e4bd390a6384946e1ae8ee8
David Kerekes <davkedav@gmail.com>
2020-08-08 15:22:03 +0200

    test: add collectCoverage

--- backend/socketServer/jest.config.js
+++ backend/socketServer/jest.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
@@ -15,7 +16,7 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
