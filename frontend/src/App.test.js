7e6d1e346b71659de298ccec6ead3c87884a738e
Ishdril <berni2015@gmail.com>
2020-08-10 12:19:31 +0200

    style: prettify previous commit

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,28 +2,25 @@ import React from 'react';
 import { render, screen, fireEvent, renderHook, cleanup } from '@testing-library/react';
 import App from './App';
 
-
 const testUsername = 'RANDOM USERNAME';
-
 
 // DOCS
 // Tests are fairly self-explanatory. check line comments
 
-describe('Integrated test App.js + Login.js', ()=> {
-
+describe('Integrated test App.js + Login.js', () => {
   //beforeEach(cleanup);
 
-  test('App renders a login',()=>{
-    render(<App/>);
+  test('App renders a login', () => {
+    render(<App />);
     expect(screen.getByTestId('login')).toBeInTheDocument();
-  })
+  });
 
   test('Integrated Testing of App.js and Login.js to see if all components of the login render properly', () => {
     const regexp = new RegExp(testUsername);
-    render(<App/>);
+    render(<App />);
 
     fireEvent.change(screen.getByRole('textbox'), {
-      target: {value: testUsername},
+      target: { value: testUsername },
     });
 
     fireEvent.click(screen.getByText('Enter'));
@@ -33,19 +30,15 @@ describe('Integrated test App.js + Login.js', ()=> {
     expect(screen.getByText('Logout')).toBeInTheDocument();
   });
 
-  test('Integrated testing of App.js and Login.js to see if logout functions properly' , () => {
+  test('Integrated testing of App.js and Login.js to see if logout functions properly', () => {
     // we are already logged in due to test above
     const regexp = new RegExp(testUsername);
-    render(<App/>);
+    render(<App />);
 
     // now logout. We didn't unmount anything so the state is as is from previous test()
     fireEvent.click(screen.getByText('Logout'));
 
     expect(screen.getByTestId('login')).toBeInTheDocument();
-    expect( () =>  getByText(regexp)).toThrow();
+    expect(() => getByText(regexp)).toThrow();
   });
-})
-
-
-
-
+});
--- frontend/src/components/Login/Login.test.js
+++ frontend/src/components/Login/Login.test.js
@@ -9,8 +9,6 @@ import Login from './Login';
 
 // Further development: verify behavior of box upon window-resizing/use in different devices.
 
-
-
 describe('<login />', () => {
   test('a login should be rendered', () => {
     render(<Login />);
@@ -22,52 +20,36 @@ describe('<login />', () => {
     const testUsername = 'random username';
     render(<Login />);
     fireEvent.change(screen.getByRole('textbox'), {
-      target: {value: testUsername},
+      target: { value: testUsername },
     });
     // getByDisplay value grabs the input in the box
     expect(screen.getByDisplayValue(testUsername)).toBeInTheDocument();
-  })
+  });
 
   test('On submit should persist submission', () => {
     const setName = jest.fn();
     const testUsername = 'random username';
 
-
-    render(<Login setName={setName}/>);
+    render(<Login setName={setName} />);
 
     fireEvent.change(screen.getByRole('textbox'), {
-      target: {value: testUsername},
+      target: { value: testUsername },
     });
     fireEvent.click(screen.getByText('Enter'));
 
     //expect(setName.mock.calls.length).toBe(1);
     expect(setName).toHaveBeenCalledTimes(1);
-
   });
-
 
   // https://medium.com/wehkamp-techblog/unit-testing-your-react-application-with-jest-and-enzyme-81c5545cee45
   test('On submit without text inputed, fires alert', () => {
     //jest.clearAllMocks();
     const setName = jest.fn();
-    window.alert = jest.fn();// = jest.spyOn(window, 'alert');
+    window.alert = jest.fn(); // = jest.spyOn(window, 'alert');
 
-    render(<Login setName={setName}/>);
+    render(<Login setName={setName} />);
     fireEvent.click(screen.getByText('Enter'));
     expect(setName).toHaveBeenCalledTimes(0);
     expect(window.alert).toHaveBeenCalledTimes(1);
-
-
   });
-
-
-
-
-
-  // test('user input should be stored properly', ()=>{
-  //   render(<App />);
-
-  // })
-
-
 });
