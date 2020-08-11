1a38a3c3318b56bfd1886df76dc4a8d130d75c39
David Kerekes <davkedav@gmail.com>
2020-08-11 17:08:28 +0200

    test(lukisLukis.js): add background image check

--- frontend/cypress/integration/lukisLukis.js
+++ frontend/cypress/integration/lukisLukis.js
@@ -3,4 +3,11 @@ describe('My First Test', () => {
   it('Visit Lukis Lukis', () => {
     cy.visit('http://localhost:3001');
   });
+
+  it('Check if hero background image is loaded properly', () => {
+    cy.get('img').then(($img) => {
+      expect($img[0].naturalWidth).to.be.greaterThan(0)
+      expect($img[0].naturalHeight).to.be.greaterThan(0)
+    });
+  });
 });
