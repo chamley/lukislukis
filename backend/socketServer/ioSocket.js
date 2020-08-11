8581e1481f280eb62f61b41f7670592b6cb7271c
Ishdril <berni2015@gmail.com>
2020-08-11 17:35:42 +0200

    feat(socketServer): Removed lockby and all lock functions. They're not used in the FE any more

--- backend/socketServer/ioSocket.js
+++ backend/socketServer/ioSocket.js
@@ -4,7 +4,6 @@ function makeIoServer(httpServer) {
   const io = socketIo(httpServer);
 
   let users = []; // object with id: string, name: string;
-  let lockby = {}; // object with id:, timestamp
 
   const removeUser = (socket) => users.filter((user) => user.id !== socket.id);
 
@@ -14,9 +13,7 @@ function makeIoServer(httpServer) {
     socket.on('login', () => {});
 
     socket.on('save', (msg) => {
-      lockby = {};
       socket.broadcast.emit('saving', msg);
-      socket.broadcast.emit('locks', lockby);
     });
 
     socket.on('enter', (name) => {
@@ -29,39 +26,13 @@ function makeIoServer(httpServer) {
     });
 
     socket.on('leave', () => {
-      if (socket.id === lockby.id) {
-        lockby = {};
-        socket.broadcast.emit('locks', lockby);
-      }
       users = removeUser(socket);
       socket.broadcast.emit('userList', users);
     });
 
     socket.on('disconnect', () => {
-      if (socket.id === lockby.id) {
-        lockby = {};
-        socket.broadcast.emit('locks', lockby);
-      }
       users = removeUser(socket);
       socket.broadcast.emit('userList', users);
-    });
-
-    socket.on('lock', (name) => {
-      lockby = {
-        id: socket.id,
-        name,
-        time: Date.now(),
-      };
-      socket.broadcast.emit('locks', lockby);
-    });
-
-    socket.on('unlock', () => {
-      lockby = {};
-      socket.broadcast.emit('locks', lockby);
-    });
-
-    socket.on('getLocks', () => {
-      socket.emit('locks', lockby);
     });
   });
 
