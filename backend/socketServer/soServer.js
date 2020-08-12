7277d170f159e32dcc51c6f8a9bb2152f164541d
schamley <schamley@bu.edu>
2020-08-12 19:53:53 +0200

    asdf

--- backend/socketServer/soServer.js
+++ backend/socketServer/soServer.js
@@ -1,11 +1,29 @@
+
 const app = require('express')();
-const http = require('http').createServer(app);
+const httpmodule = require('http');
+const cors = require('cors');
 const makeIoServer = require('./ioSocket');
 
+const IO_PORT = process.env.IO_PORT || 4000;
+
+// app.use something something
+
+app.use(cors({
+  credentials:true,
+  origin: 'https://lukis-lukis-server.herokuapp.com/'
+}));
+
+
+
+/// 
+const http = httpmodule.createServer(app);
 const io = makeIoServer(http);
+
+//give io a handle request
+
 io.on('connect_failed', () => console.error('Connection failed!'));
 
-http.listen( 4000, () => console.info('listening on *:4000'));
+http.listen(IO_PORT, () => console.info('listening on *:4000'));
 
 
 
