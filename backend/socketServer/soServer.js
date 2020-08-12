3aedcdca7f21543035b90665b50b77ff051cdde9
schamley <schamley@bu.edu>
2020-08-12 20:19:58 +0200

    move

--- backend/socketServer/soServer.js
+++ backend/socketServer/soServer.js
@@ -10,7 +10,7 @@ const IO_PORT = process.env.IO_PORT || 4000;
 
 app.use(cors({
   credentials:true,
-  origin: 'https://lukis-lukis-server.herokuapp.com/'
+  origin: 'https://lukis-lukis.herokuapp.com/'
 }));
 
 
