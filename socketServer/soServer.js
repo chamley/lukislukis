52462c5ee89d1e05b57adc40e9be1160c376a27d
schamley <schamley@bu.edu>
2020-08-13 00:48:18 +0200

    3

--- socketServer/soServer.js
+++ socketServer/soServer.js
@@ -13,7 +13,7 @@ app.use(cors({
   origin: 'https://lukis-lukis.herokuapp.com'
 }));
 app.use( (req,res,next)=> {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*:*");
 })
 
 const http = httpmodule.createServer(app);
