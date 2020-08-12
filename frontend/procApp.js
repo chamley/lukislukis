cc109b60840b5a56e47a1f396a00b61ffd3fc00b
schamley <schamley@bu.edu>
2020-08-12 17:48:12 +0200

    add front end proc script

---
+++ frontend/procApp.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const http = require('http');
+const path = require('path');
+
+var app = express;
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (_, res) => {
+  res.sendFile(path.join(__dirname, './build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.set('port', PORT);
+const server = http.createServer(app);
+
+/* eslint-disable-next-line no-console */
+server.listen(PORT, () => console.log('IO running on: ', PORT));
+
+
+
+
+
+
