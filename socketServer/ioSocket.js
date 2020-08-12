2f42c78e3ecabd3dabfbee4c11999fb3e9126559
schamley <schamley@bu.edu>
2020-08-13 01:04:34 +0200

    3

--- socketServer/ioSocket.js
+++ socketServer/ioSocket.js
@@ -1,7 +1,17 @@
 const socketIo = require('socket.io');
 
 function makeIoServer(httpServer) {
-  const io = socketIo(httpServer);
+  const io = socketIo(httpServer, {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+  });
   io.origins(['http://lukis-lukis.herokuapp.com']);
 
   let users = []; // object with id: string, name: string;
