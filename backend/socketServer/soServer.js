492f99eb4d31e8aa645f60ebcd13749702db8e24
schamley <schamley@bu.edu>
2020-08-12 20:53:27 +0200

    io

--- backend/socketServer/soServer.js
+++ backend/socketServer/soServer.js
@@ -8,14 +8,16 @@ const IO_PORT = process.env.IO_PORT || 4000;
 
 // app.use something something
 
-app.use(cors({
-  credentials:false,
-  origin: 'https://lukis-lukis.herokuapp.com/'
-}));
+// app.use(cors({
+//   credentials:false,
+//   origin: 'https://lukis-lukis.herokuapp.com/'
+// }));
+app.use(function (req, res, next) { 
+  res.header("Access-Control-Allow-Origin", "http://lukis-lukis.herokuapp.com");
+  res.header('Access-Control-Allow-Credentials', true); 
+  next();
+});
 
-
-
-/// 
 const http = httpmodule.createServer(app);
 const io = makeIoServer(http)
   .listen(IO_PORT, () => console.info('listening on *:4000'));
